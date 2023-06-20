54e8cf851550aa226250fab96d6b1651470af07a
wafae16gahboub <wafa.gahboub@edu.uiz.ac.ma>
2023-06-20 14:54:38 -0700

    erreur

--- pall.c
+++ pall.c
@@ -9,15 +9,3 @@
  * Return: Nothing
  */
 void pall(stack_t **stack, unsigned int line_number)
-{
-	char buf[1024];
-	int len = 0;
-
-	for (stack_t *temp = *stack; temp; temp = temp->next)
-	{
-		len += sprintf(buf + len, "%d\n", temp->n);
-	}
-
-	if (len > 0)
-		fwrite(buf, 1, len, stdout);
-}
