a062fc53493ffb90838499c7187350346cd81574
SiMoG97 <echaaranimohamed@gmail.com>
2023-06-21 16:06:51 -0700

    fixing mul.c file maybe

--- mul.c
+++ mul.c
@@ -15,10 +15,11 @@ void mul(stack_t **stack, unsigned int line_number)
 	int sum = 0;
 
 	if (!(*stack) || !(*stack)->next)
-		error_handler(stack, ERROR_ADD, line_number, NULL, NULL);
+		error_handler(stack, ERROR_MUL, line_number, NULL, NULL);
+
 	top = *stack;
 	second = (*stack)->next;
-	sum = top->n + second->n;
+	sum = top->n * second->n;
 	second->n = sum;
 
 	top->next = second->next;
